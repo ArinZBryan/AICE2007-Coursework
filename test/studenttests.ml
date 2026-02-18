fc3b7091c0660197683cf0d300a9ff93fa8fa4f7
Jamas Hodivala <jh22g24@soton.ac.uk>
2026-02-18 11:45:42 +0000

    committing the x86 assembly code for part 3

--- test/studenttests.ml
+++ test/studenttests.ml
@@ -2,7 +2,7 @@ open Util.Assert
 open X86
 open Simulator
 open Gradedtests
-
+open X86.Asm
 (* You should also add additional test cases here to help you   *)
 (* debug your program.                                          *)
 
@@ -30,3 +30,32 @@ let provided_tests : suite = [
   ]);
 
 ] 
+let asm_test = [
+  text "power"
+   [
+      Cmpq,  [~$0; ~%Rsi]
+    ; J Eq,  [~$$"base"]
+    ; Subq,  [~$8; ~%Rsp]
+    ; Movq,  [~%Rdi; Ind2 Rsp]
+    ; Decq,  [~%Rsi]
+    ; Callq, [~$$"power"]
+    ; Movq,  [Ind2 Rsp; ~%Rbx]
+    ; Imulq, [~%Rbx; ~%Rax]
+    ; Addq,  [~$8; ~%Rsp]
+    ; Retq,  []
+    ]
+  ; text "base"
+    [
+      Movq,  [~$1; ~%Rax]
+    ; Retq,  []
+    ]
+  ; gtext "main"
+    [
+      Movq, [~$2; ~%Rdi]
+    ; Movq, [~$5; ~%Rsi]
+    ; Callq, [~$$"power"]
+    ; Retq,  []
+    ]
+]
+let power_test = 
+  program_test asm_test 32L
